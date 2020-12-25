ce4673562d705d228363fe8b38197dd898c8d602
Florian Kruse <fkruse@point-8.de>
2020-12-25 22:32:44 +0100

    nightwatch script

--- nightwatch.py
+++ nightwatch.py
@@ -1,4 +1,12 @@
 #!/bin/env python3
 
-import time
+import pendulum
 
+now = pendulum.now()
+night_start = pendulum.now().start_of('day').add(hours=23)
+night_end = pendulum.now().start_of('day').add(hours=5)
+
+if now <= night_end or now >= night_start:
+    print("true")
+else:
+    print("false")
