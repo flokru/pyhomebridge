313d2b41df8533cbdbad627775caa5dce09f4c79
Florian Kruse <fkruse@point-8.de>
2021-02-14 21:47:25 +0100

    colorwheel v1

--- colorwheel.py
+++ colorwheel.py
@@ -2,13 +2,49 @@
 
 from phue import Bridge
 import random
+import sys
 
-b = Bridge('192.168.0.11')
-b.connect()
-b.lights
-light = b.lights_by_name['Wohnzimmer Sideboard Stehlampe']
-light.transitiontime = 5
-light.on = True
-light.xy = (random.random(), random.random())
-light.saturation = int(random.random() * 255.0)
-light.brightness = 255
+
+def wheel():
+    b = Bridge('192.168.0.11')
+    b.connect()
+    b.lights
+    light = b.lights_by_name['Wohnzimmer Sideboard Stehlampe']
+    light.transitiontime = 5
+    light.on = True
+    light.xy = (random.random(), random.random())
+    light.saturation = int(random.random() * 255.0)
+    light.brightness = 255
+
+
+def off():
+    b = Bridge('192.168.0.11')
+    b.connect()
+    b.lights
+    light = b.lights_by_name['Wohnzimmer Sideboard Stehlampe']
+    light.on = False
+
+
+def status():
+    b = Bridge('192.168.0.11')
+    b.connect()
+    b.lights
+    light = b.lights_by_name['Wohnzimmer Sideboard Stehlampe']
+    if light.on:
+        print('true')
+    else:
+        print('false')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print('call with --on --off --status')
+    elif sys.argv[1] == '--on':
+        wheel()
+    elif sys.argv[1] == '--off':
+        off()
+    elif sys.argv[1] == '--status':
+        status()
+    else:
+        print('call with --on --off --status')
+
