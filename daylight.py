4ee382cb6ffed8aff8bd0045c87dc2478a0dfb1b
Florian Kruse <fkruse@point-8.de>
2021-02-21 17:49:58 +0100

    daylight

---
+++ daylight.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+import pendulum
+from astral.geocoder import database, lookup
+from astral.sun import sun
+
+
+
+def on():
+    pass
+
+
+def off():
+    pass
+
+
+def status():
+    now = pendulum.now()
+    night_end = pendulum.now().start_of('day').add(hours=5)
+    location = lookup("Berlin", database())
+    s = sun(location.observer, date=now)
+    if now >= night_end and now <= s['dusk']:
+        print("true")
+    else:
+        print("false")
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print('call with --on --off --status')
+    elif sys.argv[1] == '--on':
+        on()
+    elif sys.argv[1] == '--off':
+        off()
+    elif sys.argv[1] == '--status':
+        status()
+    else:
+        print('call with --on --off --status')
+
